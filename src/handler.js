5cd5a97b58e8d19eb520f2ca7e87130bdc0d616b
simple.sethia@kickdrumtech.com <simple.sethia@kickdrumtech.com>
2021-06-18 21:28:53 +0530

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@
 
 module.exports.generateRandomNumber = async (event) => {
   const randomNumber = parseInt(Math.random * 100)
-  console.log('random number is: ', randomNumber);
+  console.log('random number is:  ', randomNumber);
   return randomNumber;
 };
 
